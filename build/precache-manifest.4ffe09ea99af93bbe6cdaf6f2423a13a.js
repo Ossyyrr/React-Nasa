a7e10712cd548df6711b77a044ec2ab419e90912
Patri <ossyyrr@hotmail.com>
2020-02-17 20:33:34 +0100

    build app

---
+++ build/precache-manifest.4ffe09ea99af93bbe6cdaf6f2423a13a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318155c2e4e2ea78ad13534ef7335cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b829d58a677855f87a1",
+    "url": "/static/css/main.d00cf5da.chunk.css"
+  },
+  {
+    "revision": "9568f83fc0282de52e78",
+    "url": "/static/js/2.ead2819b.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.ead2819b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b829d58a677855f87a1",
+    "url": "/static/js/main.d10f703d.chunk.js"
+  },
+  {
+    "revision": "b80a54858ddea3aa58b7",
+    "url": "/static/js/runtime-main.d84a5610.js"
+  },
+  {
+    "revision": "65b0d38cd05995809e96028e6026098b",
+    "url": "/static/media/asteroid.65b0d38c.jpg"
+  },
+  {
+    "revision": "ba387c92aca9624c6837bc90ca657670",
+    "url": "/static/media/curiosity.ba387c92.PNG"
+  },
+  {
+    "revision": "750abb547e5d44cf30412fc9a7c60524",
+    "url": "/static/media/galaxia.750abb54.svg"
+  },
+  {
+    "revision": "10aa44d7cae36f62cca238415a20843a",
+    "url": "/static/media/today.10aa44d7.png"
+  }
+]);
